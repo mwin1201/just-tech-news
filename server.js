4fb51b91f51e6fcbf24f4222dcb2f682603fec76
Max W <mwinter@wesleyan.edu>
2022-01-11 16:25:08 -0600

    helper code

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const app = express();
