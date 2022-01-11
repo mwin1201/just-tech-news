d8b5486c8222ef729751acba121c0ed420ca69c4
Max W <mwinter@wesleyan.edu>
2022-01-11 10:56:45 -0600

    login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,7 +58,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -84,6 +92,17 @@ router.post('/login', (req, res) => {
         }
         res.json({ user: dbUserData, message: 'You are now logged in' });
     });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 
 router.put('/:id', (req, res) => {
